1fdf0b1eff7ba0ee4816d019d29aa5776e855777
Zach Milo <zachmilo@gmail.com>
2023-09-09 12:27:39 -0400

    doing something

--- backend/routes/api/groups.js
+++ backend/routes/api/groups.js
@@ -12,18 +12,20 @@ router.get('/:groupId', async (req, res) => {
     // should include array of GroupImages
     // should include Organizer info (name + id)
     // should include array of Venues
-    const group = req.params.groupId
-    res.json(group)
+    const group = await Group.findByPk(req.params.groupId);
 
+
+    res.json(group)
 })
 
 //get all groups by current user,
-router.get('/current', restoreUser, requireAuth, async (req, res) => {
-    const user = req.user;
-    console.log(user)
-    const userGroups = await Group.findByPk(user.id)
+router.get('/current', async (req, res) => {
+    // const user = req.user;
+    // console.log(user)
+    // const userGroups = await Group.findByPk(user.id)
 
-    return res.json(userGroups);
+    // return res.json(userGroups);
+
 })
 
 // get all groups
