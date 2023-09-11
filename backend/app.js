f3f4f4761faa64f72acbb778151ab7a2078537aa
Zach Milo <zachmilo@gmail.com>
2023-09-11 14:41:25 -0400

    altered error formatter

--- backend/app.js
+++ backend/app.js
@@ -75,14 +75,21 @@ app.use((err, _req, _res, next) => {
 
 // Error formatter
 app.use((err, _req, res, _next) => {
-    res.status(err.status || 500);
-    console.error(err);
+  res.status(err.status || 500);
+
+  if (isProduction) {
+    res.json({
+      message:err.message,
+      errors:err.errors
+    })
+  } else {
     res.json({
       title: err.title || 'Server Error',
       message: err.message,
       errors: err.errors,
-      stack: isProduction ? null : err.stack
+      stack: err.stack
     });
-  });
+  }
+});
 
 module.exports = app;
