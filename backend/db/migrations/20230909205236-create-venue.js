b97a14c6017076ecab20f766cdfb6f9f5b5d74c4
Zach Milo <zachmilo@gmail.com>
2023-09-11 14:31:05 -0400

    fixed seed files

--- backend/db/migrations/20230909205236-create-venue.js
+++ backend/db/migrations/20230909205236-create-venue.js
@@ -1,5 +1,10 @@
 'use strict';
-/** @type {import('sequelize-cli').Migration} */
+// /** @type {import('sequelize-cli').Migration} */
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Venues', {
@@ -39,9 +44,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Venues');
+    options.tableName = 'Venues';
+    await queryInterface.dropTable(options);
   }
 };
