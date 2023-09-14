f0ca9e1930fcfb2dcfe4023475dc0dc49f78c3ab
Zach Milo <zachmilo@gmail.com>
2023-09-14 13:48:18 -0400

    event stuff

--- backend/routes/api/events.js
+++ backend/routes/api/events.js
@@ -1,11 +1,51 @@
 const express = require('express');
 const router = express.Router();
 const { setTokenCookie, requireAuth, restoreUser } = require('../../utils/auth');
-const { User, Group, Image, Member, Venue, Event } = require('../../db/models');
+const { User, Group, Image, Member, Venue, Event, Attendance } = require('../../db/models');
 const { check } = require('express-validator');
+const { Op } = require("sequelize");
 
 
+// add image to an event
+router.post('/:eventId/images', restoreUser, requireAuth, async (req, res) => {
+    const { url, preview } = req.body;
+    const userId = req.user.id;
+    const event = await Event.findByPk(req.params.eventId);
 
+    if (!event) {
+        return res.status(404).json({
+            message: "Event couldn't be found"
+        });
+    }
+
+    const authorizedUser = await Attendance.findByPk(userId, {
+        where: {
+            [Op.or]: [{status: 'attendee'}, {status: 'host'}, {status: 'co-host'}]
+        }
+    })
+    console.log(authorizedUser, 'yoooooooooooooooooooooo')
+    if (!authorizedUser) {
+        return res.status(403).json(
+            {
+             "message": "Forbidden"
+            })
+        }
+
+    const image = await Image.create({url, preview, imageableType: 'EventImages', imageableId: req.params.eventId})
+    if (preview === true) {
+        await event.update({ previewImage: url})
+    }
+
+    const createdImage = {
+        id:image.id,
+        url:image.url,
+        preview:image.preview
+    }
+
+    return res.status(200).json(createdImage);
+})
+
+// get all events
 router.get('/', async (req, res) => {
     const allEvents = await Event.findAll({
         attributes: {
