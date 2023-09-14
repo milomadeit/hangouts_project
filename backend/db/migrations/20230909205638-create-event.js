0e9533085d74e28c02b8a98c8941760925b1e9fc
Zach Milo <zachmilo@gmail.com>
2023-09-14 16:44:34 -0400

    updatedAt and createdAt fields added to migrations

--- backend/db/migrations/20230909205638-create-event.js
+++ backend/db/migrations/20230909205638-create-event.js
@@ -60,11 +60,13 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       }
     }, options);
   },
