968c3e6264dd559e7205b1705dbbf4b6ae384881
Zach Milo <zachmilo@gmail.com>
2023-11-09 00:05:14 -0500

    event detail page styling mostly complete, just need to add onclick events and conditional logic

--- frontend/src/components/EventDetail/EventDetail.js
+++ frontend/src/components/EventDetail/EventDetail.js
@@ -40,56 +40,70 @@ function EventDetail() {
 
   const host = attendees.find((attendee) => (attendee.status = "host"));
 
+  if (sessionUser.id === event.hostId) {
+  }
+
   return (
-    <div>
-      <div className='breadcrumbLinkEvents'>
-        <p>&larr;</p>
-        <Link to='/events'>Events</Link>
-      </div>
-      <div className='upper-event-details'>
-        <div className='event-detail-thumbnail-div'>
-          <img
-            className='event-detail-thumbnail'
-            src={event.previewImage}
-            alt=' '
-          />
+    <div className=''>
+      <div className='event-detail-top'>
+        <div className='breadcrumbLinkEvents'>
+          <p>&larr;</p>
+          <Link to='/events'>Events</Link>
         </div>
-        <div className='event-detail-content'>
-          <div className='event-detail-name'>
-            <h3>{event.name}</h3>
-          </div>
-          <div className='event-detail-location'>
-            {event.Venue.city}, {event.Venue.state}
-          </div>
+        <div className='event-detail-name'>
+          <h3>{event.name}</h3>
         </div>
-        <div className='group-detail-organized-by'>
+        <div className='group-info-organizer'>
           <h5>
             Hosted by {host.firstName} {host.lastName}
           </h5>
         </div>
-        <div className='event-image-big'>
-          <img src={event.previewImage} alt=' ' />
+      </div>
+      <div className='background-div-color'>
+        <div className='upper-event-details'>
+          <div className='event-image-thumbnail-div'>
+            <img
+              className='event-image-thumbnail'
+              src={event.previewImage}
+              alt=' '
+            />
+          </div>
+          <div className='cards-div'>
+            <div className='group-info-div'>
+              <img
+                className='group-detail-image'
+                src={group.previewImage}
+                alt=' '
+              />
+              <div className='group-detail-info'>
+                <h4 className='group-detail-name'>{group.name}</h4>
+                <h6 className='group-detail-privacy'>
+                  {group.isPrivate ? "Private" : "Public"}
+                </h6>
+              </div>
+            </div>
+            <div className='event-info-card'>
+              <div className='event-info-div'>
+                <p>
+                  Start {event.startDate.slice(0, 10)}{" "}
+                  <span className='group-dot'>·</span>{" "}
+                  {event.startDate.slice(11, 16)}
+                </p>
+                <p>
+                  End {event.endDate.slice(0, 10)}{" "}
+                  <span className='group-dot'>·</span>{" "}
+                  {event.endDate.slice(11, 16)}
+                </p>
+                <p>Price: ${event.price}</p>
+                <p>{event.type}</p>
+                <div className='update-delete-buttons'>
+                  <button className="event-detail-update">Update</button>
+                  <button className="event-detail-delete">Delete</button>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div className='group-info-div'>
-          <img src={group.previewImage} />
-          <h4 className='group-detail-name'>{group.name}</h4>
-          <h7 className='group-detail-privacy'>
-            {group.isPrivate ? "Private" : "Public"}
-          </h7>
-        </div>
-        <div className='event-info-div'>
-          <p>
-            Start {event.startDate.slice(0, 10)}{" "}
-            <span className='group-dot'>·</span> {event.startDate.slice(11, 16)}
-          </p>
-          <p>
-            End {event.endDate.slice(0, 10)}{" "}
-            <span className='group-dot'>·</span> {event.endDate.slice(11, 16)}
-          </p>
-          <p>{event.price}</p>
-          <p>{event.type}</p>
-        </div>
-
         <div className='event-detail-description'>
           <h3>Details</h3>
           <p>{event.description}</p>
